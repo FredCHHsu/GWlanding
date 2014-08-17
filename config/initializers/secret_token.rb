e28a9f7b9e9a5819a3a2025c3ef19094d6b9292d
fred.c.h.hsu <fred.c.h.hsu@gmail.com>
2014-08-17 16:22:39 +0800

    GWlanding initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GWlanding::Application.config.secret_key_base = 'd4f0aa104d3b94112dfd67f1e2add108ba4f5f3ec1aff30f4929f4fee6d210d1db1c8f74fddbdf58e74654a6ceb62352a38e02f33b81ce4c0e7be0a3d1b9ea38'

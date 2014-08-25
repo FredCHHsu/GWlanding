74e58edf58e0dc0ab2452275838e4c129c10b472
fred.c.h.hsu <fred.c.h.hsu@gmail.com>
2014-08-26 00:14:13 +0800

    add _how_gw_works content

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,4 +3,10 @@ $(function() {
   $(".alert").hide().fadeIn('slow', function() {
     $(this).delay(1000).fadeOut('slow');
   });
+  $('.wodry').wodry({
+    animation: 'rotateX',
+    delay: 2000,
+    animationDuration: 800
+	});
 });
+
